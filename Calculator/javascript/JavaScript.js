a71909e618f1b41fb945c18c90700be704827aa0
Moayad Myro <moayadmyro@gmail.com>
2022-04-12 15:26:12 -0700

    Calc

---
+++ Calculator/javascript/JavaScript.js
@@ -0,0 +1,24 @@
+﻿
+const btn = document.getElementsByClassName("btnx");
+const result = document.getElementById("result");
+const eqalbtn = document.getElementById("ceqalbtn");
+const clearbtn = document.getElementById("clearbtn");
+var vale = 0;
+Array.from(btn).forEach(b => {
+    b.addEventListener("click", function (event) {
+        console.log(event.target.value);
+         vale = event.target.value;
+        result.value += vale; 
+    });
+});
+clearbtn.addEventListener("click", function (event) {
+    result.value = "";
+});
+eqalbtn.addEventListener("click", function (event) {
+    //console.log(vale);
+    //console.log(eval(result.value));
+    if (vale === "-" || vale === "*" || vale === '/' || vale === '+') {
+       
+    }
+    else {  result.value = eval(result.value);}
+});
